429d1b2db1fcaba1ea6ca0bed2785b0758372770
archanaaniyan1341 <77314710+archanaaniyan1341@users.noreply.github.com>
2021-03-09 10:16:22 +0530

    angular

---
+++ Tutorial2/src/app/server-element/server-element.component.ts
@@ -0,0 +1,40 @@
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, Input, OnChanges, OnDestroy, OnInit, SimpleChange } from '@angular/core';
+
+@Component({
+  selector: 'app-server-element',
+  templateUrl: './server-element.component.html',
+  styleUrls: ['./server-element.component.scss']
+})
+export class ServerElementComponent implements
+ OnInit,DoCheck,AfterContentInit, AfterContentChecked,AfterViewInit,
+ AfterViewChecked,OnDestroy{
+@Input('srvElement') element:{type:string,name:string,content:string}
+  constructor() { 
+    console.log('constructor called')
+  }
+ngOnChanges(changes:SimpleChange){
+  console.log('ngoNCHANGES called')
+  console.log(changes)
+}
+  ngOnInit(): void {
+    console.log('ngoninit called')
+  }
+ngDoCheck(){
+  console.log('ngdocheck called')
+}
+ngAfterContentInit(){
+  console.log('ngaftercontentinit called')
+}
+ngAfterContentChecked(){
+  console.log('ngaftercontentchecked called')
+}
+ngAfterViewInit(){
+  console.log('ngafterviewinit called')
+}
+ngAfterViewChecked(){
+  console.log('ngafterviewchecked called')
+}
+ngOnDestroy(){
+  console.log('ngONDESTROYcalled')
+}
+}
--- UdemyTutorial/src/app/recipes/recipe-list/recipe-list.component.ts
+++ UdemyTutorial/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recipe } from '../recipe.model'
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
-
+recipes:Recipe[]=[
+  new Recipe('Test','test','https://www.justataste.com/wp-content/uploads/2014/07/cheesy-leftover-mashed-potato-pancakes_recipe.jpg')
+];
   constructor() { }
 
   ngOnInit(): void {
